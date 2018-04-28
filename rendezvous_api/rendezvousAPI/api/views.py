33a0ffdca3355b8d6466483d69afba7a300a16cf
sgupta04 <satya.gupta@sema4genomics.com>
2018-04-28 12:19:33 -0400

    dockerless venv full django rest

---
+++ rendezvous_api/rendezvousAPI/api/views.py
@@ -0,0 +1,14 @@
+# api/views.py
+
+from rest_framework import generics
+from .serializers import TripSerializer
+from .models import Trip
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
