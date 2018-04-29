d213a98bd2791581a6021c98fda8b503aa8a6855
sgupta04 <satya.gupta@sema4genomics.com>
2018-04-28 21:29:40 -0400

    views updated

--- rendezvous_api/rendezvousAPI/api/urls.py
+++ rendezvous_api/rendezvousAPI/api/urls.py
@@ -4,11 +4,26 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import TripCreateView
 from .views import TripDetailsView
+from .views import SchoolCreateView
+from .views import SchoolDetailsView
+from .views import MeetupCreateView
+from .views import MeetupDetailsView
+from .views import DestinationCreateView
+from .views import DestinationDetailsView
 
 urlpatterns = {
     url(r'^trips/$', TripCreateView.as_view(), name="create"),
     url(r'^trips/(?P<pk>[0-9]+)/$',
         TripDetailsView.as_view(), name="details"),
+    url(r'^schools/$', SchoolCreateView.as_view(), name="create"),
+    url(r'^schools/(?P<pk>[0-9]+)/$',
+        SchoolDetailsView.as_view(), name="details"),
+    url(r'^meetups/$', MeetupCreateView.as_view(), name="create"),
+    url(r'^meetups/(?P<pk>[0-9]+)/$',
+        MeetupDetailsView.as_view(), name="details"),
+    url(r'^destinations/$', DestinationCreateView.as_view(), name="create"),
+    url(r'^destinations/(?P<pk>[0-9]+)/$',
+        DestinationDetailsView.as_view(), name="details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
