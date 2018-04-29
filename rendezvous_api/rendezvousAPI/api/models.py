0b1d6058ea038c67b92fd4150e9d0b6edd313d08
sgupta04 <satya.gupta@sema4genomics.com>
2018-04-29 01:04:18 -0400

    models updated

--- rendezvous_api/rendezvousAPI/api/models.py
+++ rendezvous_api/rendezvousAPI/api/models.py
@@ -9,18 +9,9 @@ class Profile(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
-class Trip(models.Model):
-    """This class represents the trip model."""
-    meetup = models.CharField(max_length=255, blank=False)
-    destination = models.CharField(max_length=255, blank=False)
-    users = models.ManyToManyField(Profile)
-    time = models.DateTimeField(default=None)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         """Return a human readable representation of the model instance."""
-        return "{}".format(self.name)
+        return "{}".format(self.user)
 
 class School(models.Model):
     """This class represents the trip model."""
@@ -49,6 +40,19 @@ class Meetup(models.Model):
     school = models.ForeignKey(School, db_column='name', on_delete=models.CASCADE)
     lat = models.CharField(max_length=255, blank=False)
     long = models.CharField(max_length=255, blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
+
+class Trip(models.Model):
+    """This class represents the trip model."""
+    meetup = models.ForeignKey(Meetup, db_column='address', on_delete=models.CASCADE)
+    destination = models.ForeignKey(Destination, db_column='train_stop', on_delete=models.CASCADE)
+    users = models.ManyToManyField(Profile)
+    time = models.DateTimeField(default=None)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
