3fc7758f99efee3f2b57bcfdcbab1ccd513dad3d
Haris <steveipkis@github.com>
2018-06-27 00:13:50 -0400

    Simple Base for Spring Boot

---
+++ rendezvous_api/src/main/java/api/rendezvous/rendezvous_api/RendezvousApiApplication.java
@@ -0,0 +1,14 @@
+package api.rendezvous.rendezvous_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendezvousApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RendezvousApiApplication.class, args);
+
+	}
+}
