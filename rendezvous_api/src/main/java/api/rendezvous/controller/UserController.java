0e1d853f6b9a01aad343f647afcfe16849457417
steveipkis <steveipkis@github.com>
2018-08-07 01:56:45 -0400

    Migrated Exception Handling to service Layer

--- rendezvous_api/src/main/java/api/rendezvous/controller/UserController.java
+++ rendezvous_api/src/main/java/api/rendezvous/controller/UserController.java
@@ -6,16 +6,11 @@ import api.rendezvous.models.User;
 import api.rendezvous.service.SchoolService;
 import api.rendezvous.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resources;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.MediaTypes;
-
 import java.util.Collection;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -25,20 +20,16 @@ public class UserController {
     private UserService userService;
 
     @Autowired
-    private SchoolService schoolService;
+    private SchoolService schoolService;        //to validate that the school exists
 
     @GetMapping
     public Collection<User> getAllUsers() {
         return this.userService.getAllUsers();
     }
 
-    @GetMapping(value = "/{user_id}", produces = MediaTypes.HAL_JSON_VALUE)
-    public Resource<User> getUserById(@PathVariable("user_id") long user_id) {
-
-        Optional<User> user = validateUser(user_id);
-        Resource<User> resource = new Resource<User>(user.get());
-        return resource;
-
+    @GetMapping(value = "/{user_id}")
+    public Optional<User> getUserById(@PathVariable("user_id") long user_id) {
+        return userService.getUserById(user_id);
     }
 
     @DeleteMapping(value = "/{user_id}")
@@ -48,51 +39,16 @@ public class UserController {
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void updateUser(@RequestBody User user) {
-        validateUser(user.getUserId());
         this.userService.updateUser(user);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void insertUser(@RequestBody User user) {
-        validateUserDoesNotExist(user.getUserId());
         this.userService.insertUser(user);
     }
 
-    @GetMapping(value = "/school/{school}", produces = MediaTypes.HAL_JSON_VALUE)
-    public Resources<Resource<User>> getUserBySchool(@PathVariable("school") long id) {
-
-        //need to run a validate school
-        validateSchool(id);
-        Collection<User> users = this.userService.getUserBySchool(id);
-        return new Resources<>(this.userService.getUserBySchool(id)
-                                                    .stream()
-                                                    .map(bookmark -> toResource(bookmark))
-                                                    .collect(Collectors.toList()));
+    @GetMapping(value = "/school/{school}")
+    public Collection<User> getUserBySchool(@PathVariable("school") long id) {
+        return this.userService.getUserBySchool(id);
     }
-
-
-    private Optional<User> validateUserDoesNotExist(long id) {
-        Optional<User> user = userService.getUserById(id);
-        if (user.isPresent())
-            throw new UserNotFoundException("User id=" + id + " already exists");
-        return user;
-    }
-
-    private Optional<User> validateUser(long id) {
-        Optional<User> user = userService.getUserById(id);
-        if (!user.isPresent())
-            throw new UserNotFoundException("User id=" + id + " does not exist");
-        return user;
-    }
-
-    private Optional<School> validateSchool(long id) {
-        Optional<School> school = schoolService.getSchoolById(id);
-        if (!school.isPresent())
-            throw new UserNotFoundException("School id=" + id + " does not exist");
-        return school;
-    }
-
-    private static Resource<User> toResource(User bookmark) {
-        return new Resource(bookmark);
-}
 }
--- rendezvous_api/src/main/java/api/rendezvous/service/UserService.java
+++ rendezvous_api/src/main/java/api/rendezvous/service/UserService.java
@@ -1,5 +1,8 @@
 package api.rendezvous.service;
 
+import api.rendezvous.exception.UserNotFoundException;
+import api.rendezvous.models.School;
+import api.rendezvous.repository.SchoolRepository;
 import api.rendezvous.repository.UserRepository;
 import api.rendezvous.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,32 +19,60 @@ public class UserService {
     @Qualifier("MySql")
     private UserRepository userrepository;
 
+    @Autowired
+    @Qualifier("MySql")
+    private SchoolRepository schoolrepository;
 
     public Collection<User> getAllUsers() {
         return (Collection<User>) this.userrepository.findAll();
     }
 
     public Optional<User> getUserById(long user_id) {
-        //do a check to see if user actually exists.
-        //if so return the value else return an error code
-        return this.userrepository.findById(user_id);
+        Optional<User> user = validateUser(user_id);
+        return user;
     }
 
     public void deleteUserById(long user_id) {
-        this.userrepository.deleteById(user_id);
+        Optional<User> user = validateUser(user_id);
+        if(user.isPresent()) {
+            this.userrepository.deleteById(user_id);
+        }
     }
 
-    public void updateUser(User user) { this.userrepository.save(user);
+    public void updateUser(User user) {
+        boolean exists = validateUser(user.getUserId()).isPresent();
+        if(exists) {
+            this.userrepository.save(user);
+        }
     }
     public void insertUser(User user) {
+        validateUserDoesNotExist(user.getUserId());
         this.userrepository.save(user);
     }
 
-    public Collection<User> getUserBySchool(long id) { return this.userrepository.findBySchoolId(id); }
-//
-//    private void validateUser(long userId) {
-//        if (this.userrepository.findById(userId) == null) {
-//            throw new UserNotFoundException(userId);
-//        }
-//    }
+    public Collection<User> getUserBySchool(long id) {
+        validateSchool(id);
+        return this.userrepository.findBySchoolId(id);
+    }
+
+    private Optional<User> validateUser(long id) {
+        Optional<User> user = userrepository.findById(id);
+        if (!user.isPresent())
+            throw new UserNotFoundException("User id=" + id + " does not exist");
+        return user;
+    }
+
+    private void validateUserDoesNotExist(long id) {
+        Optional<User> user = userrepository.findById(id);
+        if (user.isPresent())
+            throw new UserNotFoundException("User id=" + id + " already exists. Cannot insert into DB");
+
+    }
+
+    private Optional<School> validateSchool(long id) {
+        Optional<School> school = schoolrepository.findById(id);
+        if (!school.isPresent())
+            throw new UserNotFoundException("School id=" + id + " does not exist");
+        return school;
+    }
 }
